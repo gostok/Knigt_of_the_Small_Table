44496514e70500dba431782eb421c1fbf8228e09
gostok <stiv4ik12012@gmail.com>
2024-09-06 17:49:19 +0300

    state_machina
    Добавил базовую машину состояния для Плеера; добавил свет и тень Плеера.

--- characters/player/scripts/player.gd
+++ characters/player/scripts/player.gd
@@ -7,25 +7,28 @@ enum {
 var state = MOVE
 var gold = 0
 var health = 100
-const SPEED = 40
-const JUMP = -300.0
-@onready var anim = $AnimatedSprite2D
+const SPEED = 100
+const JUMP = -400.0
+var combo = false
+var attack_cooldown = false
+@onready var anim = $Animation/AnimatedSprite2D
+@onready var animPlayer = $Animation/AnimationPlayer
 
 var gravity = ProjectSettings.get_setting('physics/2d/default_gravity')
 
 func _ready() -> void:
-	anim.play('Idle')
+	pass
 
 func _physics_process(delta):
 	match  state:
 		MOVE: move_state()
-		ATTACK_1: pass
-		ATTACK_2: pass
-		ATTACK_3: pass
-		ATTACK_4: pass
-		ATTACK_AIR: pass
-		SLIDE: pass
-		ROLL: pass
+		ATTACK_1: attack_1_state()
+		ATTACK_2: attack_2_state()
+		ATTACK_3: attack_3_state()
+		ATTACK_4: attack_4_state()
+		ATTACK_AIR: attack_air_state()
+		SLIDE: slide_state()
+		ROLL: roll_state()
 		DEATH: death_state()
 		HEALTH: pass
 		CLIMB: pass
@@ -33,15 +36,15 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP
-		anim.play('jump')
+		animPlayer.play('jump')
 
 	move_and_slide()
 
 
 func move_state():
-	var direction = Input.get_axis('ui_left', 'ui_right')
+	var direction = Input.get_axis('left', 'right')
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
@@ -52,18 +55,81 @@ func move_state():
 			anim.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play('Idle')
+		if velocity.y == 0:
+			anim.play('Idle')
+	
+	if Input.is_action_pressed('roll'):
+		state = ROLL
+	if Input.is_action_pressed('slide'):
+		state = SLIDE
+	if Input.is_action_just_pressed('attack') and attack_cooldown == false:
+		state = ATTACK_1
+	if Input.is_action_just_pressed("attack") and not is_on_floor():
+		state = ATTACK_AIR
 
 func death_state():
 	if health <= 0:
 		health = 0
-		anim.play('death')
+		animPlayer.play('death')
 		await anim.animation_finished
 		queue_free()
 		get_tree().change_scene_to_file("res://all_scenes/all_menu/main_menu/main_menu.tscn")
 
 func slide_state():
-	pass
+	animPlayer.play('slide')
+	if Input.is_action_just_released('slide'):
+		state = MOVE
 
 func roll_state():
-	pass
+	velocity.x = 0
+	animPlayer.play('roll')
+	if Input.is_action_just_released('roll'):
+		state = MOVE
+
+func attack_1_state():
+	if Input.is_action_just_pressed('attack') and combo == true:
+		state = ATTACK_2
+	velocity.x = 0
+	animPlayer.play('attack_1')
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = MOVE
+
+func attack_2_state():
+	if Input.is_action_just_pressed("attack") and combo == true:
+		state = ATTACK_3
+	velocity.x = 0
+	animPlayer.play('attack_2')
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = MOVE
+
+func attack_3_state():
+	if Input.is_action_just_pressed("attack") and combo == true:
+		state = ATTACK_4
+	velocity.x = 0
+	animPlayer.play('attack_3')
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = MOVE
+
+func attack_4_state():
+	velocity.x = 0
+	animPlayer.play('attack_4')
+	await animPlayer.animation_finished
+	state = MOVE
+
+func attack_air_state():
+	velocity.x = 0
+	animPlayer.play("attack_air")
+	await animPlayer.animation_finished
+	state = MOVE
+
+func combo1():
+	combo = true
+	await animPlayer.animation_finished
+	combo = false
+func attack_freeze():
+	attack_cooldown = true
+	await get_tree().create_timer(0.8).timeout
+	attack_cooldown = false
